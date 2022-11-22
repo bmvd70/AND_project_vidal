38bb7635eef41ecb1b159d1e492fa34f048b9ff2
bmvd70 <bastian.vidal@mni.thm.de>
2022-11-22 22:06:32 +0100

    established connection to firebase auth, create user works

--- app/src/main/java/com/example/and_vidal/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/and_vidal/ui/login/LoginFragment.java
@@ -4,8 +4,8 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +13,12 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.and_vidal.R;
 import com.example.and_vidal.databinding.FragmentLoginBinding;
-import com.example.and_vidal.databinding.FragmentProfileBinding;
-import com.example.and_vidal.ui.profile.ProfileFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -32,10 +34,11 @@ public class LoginFragment extends Fragment {
     private FragmentLoginBinding binding;
 
     // TODO: Rename and change types of parameters
+    private static final String TAG = "CustomAuthActivity";
     private String mParam1;
     private String mParam2;
-
-
+    private FirebaseAuth mAuth;
+    private String mCustomToken;
 
     /**
      * Use this factory method to create a new instance of
@@ -62,6 +65,15 @@ public class LoginFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
 
     @Override
@@ -79,7 +91,7 @@ public class LoginFragment extends Fragment {
                 TextView edEmail = binding.username;
                 String email = edEmail.getText().toString().trim();
                 String password = edPassword.getText().toString().trim();
-                Toast.makeText(getActivity(), "Button was clicked", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getActivity(), "Button was clicked", Toast.LENGTH_SHORT).show();
                 if (password.isEmpty()) {
                     edPassword.setError("Password is required!");
                     edPassword.requestFocus();
@@ -96,6 +108,7 @@ public class LoginFragment extends Fragment {
                     edEmail.requestFocus();
                     return;
                 }
+                startSignUp();
             }
         });
 
@@ -107,5 +120,57 @@ public class LoginFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void startSignUp() {
+        TextView edPassword = binding.password;
+        TextView edEmail = binding.username;
+        String email = edEmail.getText().toString().trim();
+        String password = edPassword.getText().toString().trim();
+        Toast.makeText(getActivity(), "inside Signup", Toast.LENGTH_SHORT).show();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+    private void startSignIn() {
+        // [START sign_in_custom]
+        mAuth.signInWithCustomToken(mCustomToken)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCustomToken:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END sign_in_custom]
+    }
+
+    private void updateUI(FirebaseUser user) {
+        Log.d(TAG, "updateUI:" + user);
     }
 }
