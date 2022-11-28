15509eb5896e76d7b42953ad23eeae418ef842a7
bmvd70 <bastian.vidal@mni.thm.de>
2022-11-28 12:49:37 +0100

    navbar color

--- app/src/main/java/com/example/and_vidal/MainActivity.java
+++ app/src/main/java/com/example/and_vidal/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.and_vidal;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,10 +17,15 @@ import com.example.and_vidal.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Window window;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        window = getWindow();
+        //window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getResources().getColor(R.color.bg, null));
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
